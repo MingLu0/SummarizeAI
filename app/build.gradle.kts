5671cb1d4aceab8b6d7d7f7894858eadf9e71b2e
Ming Luo <luoming886@gmail.com>
2025-11-29 00:44:22 +1300

    Bump version to 1.2.10 (versionCode 27)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.nutshell"
         minSdk = 24
         targetSdk = 34
-        versionCode = 26
-        versionName = "1.2.9"
+        versionCode = 27
+        versionName = "1.2.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
